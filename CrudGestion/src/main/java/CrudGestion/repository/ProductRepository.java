33b555763bfd30b474b4de42e15289e5f0a73608
Luishi1 <luis.oviedo.et26@gmail.com>
2024-09-09 05:39:03 -0300

    prueba y error

--- CrudGestion/src/main/java/CrudGestion/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package CrudGestion.repository;
-import CrudGestion.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
